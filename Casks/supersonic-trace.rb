82afde72c6cebd88262add748b75c8e56a9afea9
Lubos Buracinsky <lubos@komfi.health>
2026-06-18 17:58:31 +0200

    Fix Supersonic Trace cask URL

--- Casks/supersonic-trace.rb
+++ Casks/supersonic-trace.rb
@@ -2,12 +2,12 @@ cask "supersonic-trace" do
   version "0.2.1"
   sha256 "c291177d6018f50578bd60f9f009bf5d37a78aab5ba14f91d6e3e60a021e0960"
 
-  url "https://github.com/lubos-buracinsky/Supersonic-Trace/releases/download/v#{version}/Supersonic-Trace-#{version}.zip"
+  url "https://github.com/lubos-buracinsky/supersonic-trace/releases/download/v#{version}/Supersonic-Trace-#{version}.zip"
   name "Supersonic Trace"
   desc "Private macOS clipboard for developers and AI agents"
   homepage "https://supersonic.xyz/supersonic-trace/"
 
-  depends_on macos: ">= :sonoma"
+  depends_on macos: :sonoma
 
   app "Supersonic Trace.app"
   binary "#{appdir}/Supersonic Trace.app/Contents/Resources/trace"
